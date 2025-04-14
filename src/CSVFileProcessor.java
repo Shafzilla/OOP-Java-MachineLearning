50446ab934d4e1380551594c2e53b81567d2b52a
Shafzilla <74687975+Shafzilla@users.noreply.github.com>
2025-04-14 19:28:09 +0100

    added printFileContents method to print the contents of the CSV file

--- src/CSVFileProcessor.java
+++ src/CSVFileProcessor.java
@@ -1,5 +1,49 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class CSVFileProcessor
 {
+    private String file;
+    private BufferedReader reader;
 
+    public CSVFileProcessor()
+    {
+        file = "application_data.csv";
+
+    }
+
+    public void printFileContents()
+    {
+        String line = "";
+
+        try {
+            reader = new BufferedReader(new FileReader(file));
+            while((line = reader.readLine()) != null){
+
+                String[] row = line.split(",");
+
+                for(String index : row)
+                {
+                    System.out.printf("%-10s", index);
+                }
+                System.out.println();
+
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+
+        }
+        finally {
+            try {
+                reader.close();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,13 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args)
     {
 
-        String file = "application_data.csv";
-        BufferedReader reader = null;
-        String line = "";
-
-        try {
-            reader = new BufferedReader(new FileReader(file));
-            while((line = reader.readLine()) != null){
-
-                String[] row = line.split(",");
-
-                for(String index : row)
-                {
-                    System.out.printf("%-10s", index);
-                }
-                System.out.println();
-
-            }
-        }
-        catch(Exception e){
-            e.printStackTrace();
-
-        }
-        finally {
-            try {
-
-            }
-
-        }
-
+        CSVFileProcessor fileProcess = new CSVFileProcessor();
+        fileProcess.printFileContents();
 
     }
 }
